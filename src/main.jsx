0c7786cd15f6da838c27b522882b4a53a51610d5
Your Name <your-github-email@example.com>
2026-06-24 17:44:51 +0530

    Updated : When you refresh, the browser jumps back to that section.

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+// Disable browser scroll restoration
+if ("scrollRestoration" in window.history) {
+window.history.scrollRestoration = "manual";
+}
+
+window.onbeforeunload = () => {
+window.scrollTo(0, 0);
+};
+
 ReactDOM.createRoot(document.getElementById("root")).render(
 <React.StrictMode> <App />
 </React.StrictMode>
